1002896757195f4e8563dd52df4aafe7774a34d8
Christine <splogy2001@hotmail.com>
2017-09-10 16:54:07 +0100

    Not quite but closeish

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,13 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+
+
+def get_squares(arr)
+	arr2 = arr.map { |n| n * n }
+	arr3 = (arr2 & arr)
+	arr4 = arr3.map { |n| Math.sqrt(n).to_i}
+	arr4.sort
+
+end
