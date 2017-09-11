878632b6d31e78ee5eb8c03ed9bec4ee3a72cec3
Christine <splogy2001@hotmail.com>
2017-09-11 15:12:12 +0100

    Not returning what the solution says it should, but I think answering the question more accuratly than the solution answer

--- session3/challenge/3_hashes.rb
+++ session3/challenge/3_hashes.rb
@@ -6,3 +6,16 @@
 #
 # Example:
 # word_count "The dog and the cat" # => {"the" => 2, "dog" => 1, "and" => 1, "cat" => 1}
+
+
+
+
+def word_count(str)
+	arry = str.split( )
+	arr2 = arry.select { |word| word[word.downcase] }
+	counts = Hash.new(0)
+	arr2.each {|n| counts[n] += 1}
+	print counts
+end
+
+word_count( "The dog and the cat and the sheep")
